66ca53c4614ce6ccbf3ddf009ff128d2c42d3a0c
Tiran Manukyan <manukyan.tiran@gmail.com>
2022-02-11 05:58:56 +0400

    User data migration.

---
+++ src/main/resources/db/migration/V3__data_migration_inserting_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO "user" (email, password)
+VALUES ('test1@mail.com', '$2a$10$MgWcY8iCGzxP3cgw/.i0XeycUPIsufQuzw4nGTqbQOhWV4SQI8ABe'), -- test_pass1
+       ('test2@mail.com', '$2a$10$Tp2e7MY6OhKF7NLPmERkSuFU1yS1jQWGPVbewHaSr7aOtqg0sBRsC'), -- test_pass2
+       ('test3@mail.com', '$2a$10$YsFOVGivNFznCCkozXBf2OyBkUGAfMP0cybqkPZKtmxuBS0R6.vcS'); -- test_pass3
